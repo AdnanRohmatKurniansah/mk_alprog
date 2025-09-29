1a6808f7b88f1bcc8575d5cf660240e41590dcd8
AdnanRohmatKurniansah <atnandimas@gmail.com>
2025-09-29 21:59:27 +0700

    piramid

---
+++ teori/latihan_1/Piramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 9; 
+
+    for (int i = 1; i <= n; i++) {
+        // cetak spasi
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // cetak bintang
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            cout << "*";
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
